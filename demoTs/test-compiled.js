93f52e6866355d3536d529aef512bd336e2b473a
yanghui <sundayyh@qq.com>
2017-05-30 19:20:22 +0800

    "demo"11
    (cherry picked from commit 41dca9e097b45d88a1d8a535b79d3c95453c5e67)

---
+++ demoTs/test-compiled.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/**
+ * Created by yanghui on 2017/1/7.
+ */
+
+console.log(123);
+console.log(123);
+console.log(123);
+console.log(123);
+
+//# sourceMappingURL=test-compiled.js.map
